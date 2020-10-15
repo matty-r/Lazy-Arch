c6d2a5d16f2dc28ff1ca79db60af3dd1ba059ccf
matty-r <dev@rhoatech.com>
2020-10-15 17:07:42 +1000

    update bundle configs

--- bundleConfigurators.sh
+++ bundleConfigurators.sh
@@ -20,31 +20,13 @@ qemuGuestPackages-Config(){
 }
 
 kdePackages-Config(){
-
   #enable SDDM and set autologin, also set theme to breeze
   sudo systemctl enable sddm
   sudo mkdir /etc/sddm.conf.d
 
-  cat << EOF | sudo tee -a /etc/sddm.conf.d/kde_settings.conf
-[Autologin]
-Relogin=false
-Session=plasma
-User=
-
-[General]
-HaltCommand=/usr/bin/systemctl poweroff
-RebootCommand=/usr/bin/systemctl reboot
-
-[Theme]
-Current=breeze
-
-[Users]
-MaximumUid=60000
-MinimumUid=1000
-EOF
-
-  sudo sed -i 's|^User=.*|User='${USERVARIABLES[USERNAME]}'|' /etc/sddm.conf.d/kde_settings.conf
-
+  sudo kwriteconfig5 --file /etc/sddm.conf.d/kde_settings.conf --group Autologin --key Session plasma
+  sudo kwriteconfig5 --file /etc/sddm.conf.d/kde_settings.conf --group Autologin --key User ${USERVARIABLES[USERNAME]}
+  sudo kwriteconfig5 --file /etc/sddm.conf.d/kde_settings.conf --group Theme --key Current breeze
 }
 
 gnomePackages-Config(){
@@ -91,155 +73,91 @@ themePackages-Config(){
   sudo sed -i 's|^#GRUB_ENABLE_CRYPTODISK=y.*|GRUB_ENABLE_CRYPTODISK=y|' /etc/default/grub
   sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+  ## Installed the tiled menu
+  curl -L -O https://github.com/Zren/plasma-applet-tiledmenu/archive/v40.zip 
+  unzip v40.zip
+  cd ~/plasma-applet-tiledmenu-40/
+  kpackagetool5 -t Plasma/Applet -i package
+  sed -i 's|^plugin=org.kde.plasma.taskmanager.*|plugin=com.github.zren.tiledmenu|' ~/.config/plasma-org.kde.plasma.desktop-appletsrc
+  sed -i 's|^org.kde.plasma.taskmanager.*|plugin=org.kde.plasma.icontasks|' ~/.config/plasma-org.kde.plasma.desktop-appletsrc
+
+  #kwriteconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 22 --group Applets --group 41 --key immutability 1
+  #kwriteconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 22 --group Applets --group 41 --key plugin com.github.zren.tiledmenu
+
+  #kwriteconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 22 --group Applets --group 41 --group Configuration --group General --key tileModel '=W10='
+
+  #kwriteconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 22 --group Applets --group 41 --group Shortcuts --key global Alt+F1
+
+  #kwriteconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 22 --group Applets --group 42 --key immutability 1 
+  #kwriteconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 22 --group Applets --group 42 --key plugin org.kde.plasma.icontasks
+
+
   ##temporary fix for the dark theme colours being incorrect
   sudo cp /usr/share/color-schemes/Qogirdark.colors /usr/share/plasma/desktoptheme/Qogir-dark/colors
 
-##Apply the themes to qogir and icons set to numix
-cat << EOF | tee ~/.config/kdeglobals
-[General]
-ColorScheme=Qogir
-Name=Breeze
-XftHintStyle=hintslight
-XftSubPixel=rgb
-shadeSortColumn=true
-toolBarFont=Noto Sans,10,-1,5,50,0,0,0,0,0
+  ##Apply the themes to qogir and icons set to numix
+  kwriteconfig5 --file ~/.config/kdeglobals --group General --key ColorScheme Qogir
+  kwriteconfig5 --file ~/.config/kdeglobals --group General --key Name Breeze
+  kwriteconfig5 --file ~/.config/kdeglobals --group Icons --key Theme Numix-Circle
+  kwriteconfig5 --file ~/.config/kdeglobals --group KDE --key widgetStyle Breeze
 
-[Icons]
-Theme=Numix-Circle
+  kwriteconfig5 --file ~/.kde4/share/config/kdeglobals --group General --key ColorScheme Qogir
+  kwriteconfig5 --file ~/.kde4/share/config/kdeglobals --group General --key Name Qogir
+  kwriteconfig5 --file ~/.kde4/share/config/kdeglobals --group General --key widgetStyle Breeze
+  kwriteconfig5 --file ~/.kde4/share/config/kdeglobals --group Icons --key Theme Numix-Circle
 
-[KDE]
-AnimationDurationFactor=0.5
-ShowDeleteCommand=false
-contrast=0
-widgetStyle=Breeze
-EOF
+  kwriteconfig5 --file ~/.config/kwinrc --group Plugins --key magiclampEnabled true
+  kwriteconfig5 --file ~/.config/kwinrc --group Plugins --key kwin4_effect_squashEnabled false
+  
+  kwriteconfig5 --file ~/.config/kwinrc --group TabBox --key BorderActivate 9
+  kwriteconfig5 --file ~/.config/kwinrc --group TabBox --key DesktopLayout org.kde.breeze.desktop
+  kwriteconfig5 --file ~/.config/kwinrc --group TabBox --key DesktopListLayout org.kde.breeze.desktop
+  kwriteconfig5 --file ~/.config/kwinrc --group TabBox --key LayoutName org.kde.breeze.desktop
 
-cat << EOF | tee ~/.kde4/share/config/kdeglobals
-[General]
-ColorScheme=Qogir
-Name=Qogir
-font=Noto Sans,10,-1,5,50,0,0,0,0,0
-menuFont=Noto Sans,10,-1,5,50,0,0,0,0,0
-shadeSortColumn=true
-smallestReadableFont=Noto Sans,8,-1,5,50,0,0,0,0,0
-toolBarFont=Noto Sans,10,-1,5,50,0,0,0,0,0
-widgetStyle=Breeze
+  kwriteconfig5 --file ~/.config/kwinrc --group org.kde.kdecoration2 --key BorderSize None
+  kwriteconfig5 --file ~/.config/kwinrc --group org.kde.kdecoration2 --key BorderSizeAuto false
+  kwriteconfig5 --file ~/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnRight IAX
+  kwriteconfig5 --file ~/.config/kwinrc --group org.kde.kdecoration2 --key library org.kde.kwin.aurorae
+  kwriteconfig5 --file ~/.config/kwinrc --group org.kde.kdecoration2 --key theme __aurorae__svg__Qogir
 
-[Icons]
-Theme=Numix-Circle
-EOF
+  ##Settings / Plasma Style
+  kwriteconfig5 --file ~/.config/plasmarc --group Theme --key name Qogir-dark
+  
+  ##Settings / Screen Locker
+  kwriteconfig5 --file ~/.config/kscreenlockerrc --group Greeter --key Theme com.github.vinceliuice.Qogir
+  kwriteconfig5 --file ~/.config/kscreenlockerrc --group Greeter --key WallpaperPlugin org.kde.potd
+  kwriteconfig5 --file ~/.config/kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.potd --group General --key Category 1065374
+  kwriteconfig5 --file ~/.config/kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.potd --group General --key Provider unsplash
 
-cat << EOF | tee ~/.config/kwinrc
-[Compositing]
-OpenGLIsUnsafe=false
+  ##Settings / Application Style / GTK 3
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-application-prefer-dark-theme false
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-button-images true
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-cursor-theme-name breeze_cursors
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-decoration-layout :minimize,maximize,close
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-enable-animations true
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-fallback-icon-theme Adwaita
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-font-name 'Noto Sans, 10'
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-icon-theme-name Numix-Circle
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-menu-images true
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-modules colorreload-gtk-module
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-primary-button-warps-slider false
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-theme-name Qogir-win 
+  kwriteconfig5 --file ~/.config/gtk-3.0/settings.ini --group Settings --key gtk-toolbar-style 3
 
-[Effect-Cube]
-BorderActivateCylinder=9
-BorderActivateSphere=9
+  ##Settings / Application Style / GTK 2
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-enable-animations 1
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-cursor-theme-name '"breeze_cursors"'
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-primary-button-warps-slider 0
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-cursor-theme-neme '"breeze_cursors"'
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-font-name '"Noto Sans,  10"'
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-theme-name '"Qogir-win"'
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-icon-theme-name '"Numix-Circle"'
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-fallback-icon-theme '"Adwaita"'
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-toolbar-style 3
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-menu-images 1
+  kwriteconfig5 --file ~/.gtkrc-2.0 --group "" --key gtk-button-images 1
 
-[Effect-DesktopGrid]
-BorderActivate=7
-
-[Effect-PresentWindows]
-BorderActivate=3
-BorderActivateAll=9
-BorderActivateClass=9
-
-[ElectricBorders]
-Bottom=None
-BottomRight=None
-Right=None
-Top=None
-TopLeft=None
-TopRight=None
-
-[Plugins]
-kwin4_effect_squashEnabled=false
-magiclampEnabled=true
-
-[TabBox]
-BorderActivate=9
-DesktopLayout=org.kde.breeze.desktop
-DesktopListLayout=org.kde.breeze.desktop
-LayoutName=org.kde.breeze.desktop
-
-[Windows]
-ElectricBorderCooldown=350
-ElectricBorderCornerRatio=0.25
-ElectricBorderDelay=150
-ElectricBorderMaximize=true
-ElectricBorderTiling=true
-ElectricBorders=0
-
-[org.kde.kdecoration2]
-BorderSize=None
-BorderSizeAuto=false
-ButtonsOnLeft=
-ButtonsOnRight=IAX
-library=org.kde.kwin.aurorae
-theme=__aurorae__svg__Qogir
-EOF
-
-##create
-cat << EOF | tee ~/.config/plasmarc
-[Theme]
-name=Qogir-dark
-EOF
-
-##append
-cat << EOF | tee ~/.config/kscreenlockerrc
-[Greeter]
-Theme=com.github.vinceliuice.Qogir
-EOF
-
-##delete and create file
-cat << EOF | tee ~/.config/xsettingsd/xsettingsd.conf
-Net/ThemeName "Qogir-win"
-Gtk/EnableAnimations 1
-Gtk/DecorationLayout ":minimize,maximize,close"
-Gtk/PrimaryButtonWarpsSlider 0
-Gtk/ToolbarStyle 3
-Gtk/MenuImages 1
-Gtk/ButtonImages 1
-Gtk/CursorThemeName "breeze_cursors"
-Net/IconThemeName "Numix-Circle"
-Gtk/FontName "Noto Sans,  10"
-EOF
-
-##delete and create file
-cat << EOF | tee ~/.config/gtk-3.0/settings.ini
-[Settings]
-gtk-application-prefer-dark-theme=false
-gtk-button-images=true
-gtk-cursor-theme-name=breeze_cursors
-gtk-decoration-layout=:minimize,maximize,close
-gtk-enable-animations=true
-gtk-fallback-icon-theme=Adwaita
-gtk-font-name=Noto Sans,  10
-gtk-icon-theme-name=Numix-Circle
-gtk-menu-images=true
-gtk-modules=colorreload-gtk-module
-gtk-primary-button-warps-slider=false
-gtk-theme-name=Qogir-win
-gtk-toolbar-style=3
-EOF
-
-cat << EOF | tee ~/.gtkrc-2.0
-gtk-enable-animations=1
-
-gtk-cursor-theme-name="breeze_cursors"
-
-gtk-primary-button-warps-slider=0
-gtk-cursor-theme-neme="breeze_cursors"
-gtk-font-name="Noto Sans,  10"
-gtk-theme-name="Qogir-win"
-gtk-icon-theme-name="Numix-Circle"
-gtk-fallback-icon-theme="Adwaita"
-gtk-toolbar-style=3
-gtk-menu-images=1
-gtk-button-images=1
-EOF
-
+  qdbus org.kde.KWin /KWin reconfigure
 }
 
 #TODO
